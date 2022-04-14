d1bdb190eeab399277c1b9baef129c8f7119f531
NguyenTanPhucK11 <16520954@gm.uit.edu.vn>
2022-04-14 16:59:21 +0700

    NEW : recognize number, remove, negative, percent, comma

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-function input() {
-  document.getElementById("result").innerHTML = document.getElementById(
-    "1"
-  ).value;
+var elemResult = document.getElementById("result");
+// var checkComma = false;
 
-  console.log(document.getElementById("abc").value);
+function input(id) {
+  let result = elemResult.textContent;
+  let elemId = document.getElementById(id);
+
+  result == 0
+    ? (result = elemId.textContent)
+    : (result = result + elemId.textContent);
+  elemResult.innerHTML = result;
 }
+
+let remove = () => {
+  reset();
+  localStorage.setItem("result", result);
+};
+
+let saveResult = () => {
+  return localStorage.getItem("result", result);
+};
+
+let negative = () => {
+  return (elemResult.innerHTML = 0 - elemResult.textContent);
+};
+
+let percent = () => {
+  return (elemResult.innerHTML = elemResult.textContent / 100);
+};
+
+let comma = () => {
+  checkComma = true;
+  return (elemResult.innerHTML = elemResult.textContent + ".");
+};
+
+let reset = () => {
+  return (elemResult.innerHTML = 0);
+};
